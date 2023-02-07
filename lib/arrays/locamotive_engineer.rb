e62ac128b38dc24290859ab102b8ad3ad0866918
Candace Eckels <candace.eckels@gmail.com>
2023-02-07 11:35:02 -0700

    add_missing_stops method complete

--- lib/arrays/locamotive_engineer.rb
+++ lib/arrays/locamotive_engineer.rb
@@ -13,8 +13,17 @@ class LocomotiveEngineer
     wagons << each_wagons_id
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(*args)
+    stop_info = {}
+    args.each do |stop|
+      if stop.include?(:from) && stop.include?(:to)
+        stop_info[:from] = stop[:from]
+        stop_info[:to] = stop[:to]
+      else
+        stop_info[:stops] = stop.values 
+      end
+    end
+    stop_info[:stops] = [] if stop_info[:stops].nil?
   end
 
   def self.extend_route_information(route, more_route_information)
@@ -24,3 +33,10 @@ end
 
 p LocomotiveEngineer.generate_list_of_wagons(1, 7, 12, 3, 14, 8, 5)
 p LocomotiveEngineer.fix_list_of_wagons([2, 5, 1, 7, 4, 12, 6, 3, 13], [3, 17, 6, 15])
+# p LocomotiveEngineer.add_missing_stops({from: "New York", to: "Miami"},
+                                        # stop_1: "Washington, DC", 
+                                        # stop_2: "Charlotte", 
+                                        # stop_3: "Atlanta",
+                                        # stop_4: "Jacksonville", 
+                                        # stop_5: "Orlando")
+p LocomotiveEngineer.add_missing_stops({ from: 'New York', to: 'Philadelphia' })
