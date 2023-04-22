6fb7db9f8dc66a6d390cf3e5bda49a730e81043d
Candace Eckels <candace.eckels@gmail.com>
2023-04-21 19:13:52 -0600

    added notes to binary search

--- lib/algorithms/binary_search.rb
+++ lib/algorithms/binary_search.rb
@@ -1,16 +1,21 @@
 require 'pry'
 
 def search(array, target)
+  # min value of an array 
   min = 0
+  # max value of the array
   max = array.length - 1
+  # 
   if array.length == 0
     return "#{target} was not found"
   else
+    # i is the half the value of the array
     i = (min + max) / 2
     if target == array[i]
       return "#{target} found"
     else
       if array[i] < target
+        # recursive logic
         return search(array[i+1, max], target)
       else
         return search(array[min, i-1], target)
@@ -25,4 +30,4 @@ var_primes = [ 2, 3, 5, 7, 11, 13, 17, 19,
                83, 89, 97
               ]
 
-print search(0, 67)
+print search(var_primes, 67)
