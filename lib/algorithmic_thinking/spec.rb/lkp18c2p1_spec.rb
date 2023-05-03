0705219b95cc748150585eae2ce45010cf3287ae
Candace Eckels <candace.eckels@gmail.com>
2023-05-02 23:47:59 -0600

    Added notes to rspec testing to add 2moro

--- lib/algorithmic_thinking/spec.rb/lkp18c2p1_spec.rb
+++ lib/algorithmic_thinking/spec.rb/lkp18c2p1_spec.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 RSpec.describe 'Food Lines' do
-  describe ''
+  describe '#shortest_line(lines, ppl_to_enter)' do
+    # determine how these will solve the issue
+  end
+
+  describe '#solve(lines, num_of_lines, people_to_enter)' do
+
+  end
 end
