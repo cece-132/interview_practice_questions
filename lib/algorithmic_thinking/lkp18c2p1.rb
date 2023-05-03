d4959f276bc74bcf6232334c73d886fa544ddc1a
Candace Eckels <candace.eckels@gmail.com>
2023-05-02 23:41:24 -0600

    added spec testing for lkp18c2p1.rb

---
+++ lib/algorithmic_thinking/lkp18c2p1.rb
@@ -0,0 +1,8 @@
+# find problem at https://dmoj.ca/problem/lkp18c2p1
+require 'pry'
+require 'rspec'
+
+def shortest_line(num_food_lines, ppl_to_enter)
+  raise 'Input array has too many elements' if ppl_to_enter > 100
+  raise 'Invalid input array: element out of range' unless num_food_lines.all? { |line| line.between?(1, 100) }
+end
---
+++ lib/algorithmic_thinking/spec.rb/lkp18c2p1_spec.rb
@@ -0,0 +1,3 @@
+RSpec.describe 'Food Lines' do
+  describe ''
+end
