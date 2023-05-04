781cd8158dd843a95febe294ad253193329c16ea
Candace Eckels <candace.eckels@gmail.com>
2023-05-04 17:34:55 -0600

    Added testing for methods

--- lib/algorithmic_thinking/lkp18c2p1.rb
+++ lib/algorithmic_thinking/lkp18c2p1.rb
@@ -1,6 +1,1 @@
 # find problem at https://dmoj.ca/problem/lkp18c2p1
-
-def shortest_line(num_food_lines, ppl_to_enter)
-  # raise 'Input array has too many elements' if ppl_to_enter > 100
-  # raise 'Invalid input array: element out of range' unless num_food_lines.all? { |line| line.between?(1, 100) }
-end
--- spec/algorithmic_thinking/lkp18c2p1_spec.rb
+++ spec/algorithmic_thinking/lkp18c2p1_spec.rb
@@ -1,11 +1,22 @@
 require 'pry'
+require './lib/algorithmic_thinking/lkp18c2p1'
 
 RSpec.describe 'Food Lines' do
-  describe '#shortest_line(lines, ppl_to_enter)' do
-    # determine how these will solve the issue
-  end
 
   describe '#solve(lines, num_of_lines, people_to_enter)' do
-
+    it 'returns the lines that each person would enter' do
+      lines = [2,2,3,3,3]
+      m = 5
+      n = 3
+      expect(shortest_line_index(lines, n, m)).to eq("2\n2\n3")
+    end
+  end
+  describe '#shortest_line(lines, ppl_to_enter)' do
+    it 'returns the line each person goes into' do
+      lines = [2,2,3,3,3]
+      n = 3
+      expect(shortest_line_index(lines, n)).to eq("2\n2\n3")
+    end
+    # determine how these will solve the issue
   end
 end
