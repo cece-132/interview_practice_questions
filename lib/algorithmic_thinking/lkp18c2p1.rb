a2c33e0f25a187f5eebd9ba5a6d5f43cf6492fb9
Candace Eckels <candace.eckels@gmail.com>
2023-05-04 17:44:38 -0600

    edited code for solve and shortest line

--- lib/algorithmic_thinking/lkp18c2p1.rb
+++ lib/algorithmic_thinking/lkp18c2p1.rb
@@ -1,1 +1,18 @@
 # find problem at https://dmoj.ca/problem/lkp18c2p1
+require 'pry'
+
+def solve(lines, total_lines, new_people)
+  new_people.times do
+    shortest = shortest_line_index(lines, total_lines)
+    puts lines[shortest]
+    lines[shortest] += 1
+  end
+end
+
+def shortest_line_index(lines, total_lines)
+  shortest = 0
+  (1..total_lines).each do |line_position|
+    shortest = line_position if lines[line_position] < lines[shortest]
+  end
+  shortest
+end
