80903879081998e63afdee9edb3b0087c2d2ba9f
Candace Eckels <candace.eckels@gmail.com>
2023-05-06 16:19:00 -0600

    added testing and code file for snowflakes problem

---
+++ lib/algorithmic_thinking/cco07p2.rb
@@ -0,0 +1,2 @@
+# visit https://dmoj.ca/problem/cco07p2 to view the problem
+
---
+++ spec/algorithmic_thinking/cco07p2_spec.rb
@@ -0,0 +1,9 @@
+require 'pry'
+require './lib/algorithmic_thinking/cco07p2'
+
+RSpec.describe 'Snowflakes' do
+
+  describe '' do
+    
+  end
+end
