d1e2eca5191b5db80a3213378c786db2503dd3e4
Candace Eckels <candace.eckels@gmail.com>
2023-05-06 19:02:47 -0600

    Adjusting testing

--- spec/algorithmic_thinking/cco07p2_spec.rb
+++ spec/algorithmic_thinking/cco07p2_spec.rb
@@ -3,15 +3,16 @@ require './lib/algorithmic_thinking/cco07p2'
 
 RSpec.describe 'Snowflakes' do
 
-  describe 'compare(snowflake_count, snowflake_desciptions)' do
-    it 'returns if any two or more snowflakes are the same'
-    it 'snowflake_count cant be larger than 100_000'
-  end
-
-  describe 'snowflake class' do
+  describe '.snowflake class' do
     it 'exists and has attributes' 
     it 'each integers value cant be less than zero'
     it 'each integers value cant be more than 10_000_000'
+  end
+
+  describe '#compare(snowflake_count, snowflake_desciptions)' do
+    it 'returns response if any two or more snowflakes are the same'
+    it 'returns response if the snowflakes are unique'
+    it 'snowflake_count cant be larger than 100_000'
   end
 
 end
