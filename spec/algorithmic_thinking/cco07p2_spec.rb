169ae905d7e4c6a956f2c4a5972fccf407ac61aa
Candace Eckels <candace.eckels@gmail.com>
2023-05-06 18:20:30 -0600

    Mapping out testing

--- spec/algorithmic_thinking/cco07p2_spec.rb
+++ spec/algorithmic_thinking/cco07p2_spec.rb
@@ -3,7 +3,15 @@ require './lib/algorithmic_thinking/cco07p2'
 
 RSpec.describe 'Snowflakes' do
 
-  describe '' do
-    
+  describe 'compare(snowflake_count, snowflake_desciptions)' do
+    it 'returns if any two or more snowflakes are the same'
+    it 'snowflake_count cant be larger than 100_000'
   end
+
+  describe 'snowflake class' do
+    it 'exists and has attributes' 
+    it 'each integers value cant be less than zero'
+    it 'each integers value cant be more than 10_000_000'
+  end
+
 end
