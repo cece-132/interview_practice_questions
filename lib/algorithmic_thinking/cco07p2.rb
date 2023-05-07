613b8cd19f401c0ca9c9256419604759fd553c5d
Candace Eckels <candace.eckels@gmail.com>
2023-05-06 19:11:12 -0600

    Snowflake class

--- lib/algorithmic_thinking/cco07p2.rb
+++ lib/algorithmic_thinking/cco07p2.rb
@@ -1,2 +1,15 @@
 # visit https://dmoj.ca/problem/cco07p2 to view the problem
 
+def compare(snowflake_count, snowflakes)
+  snowflake_objects = snowflakes.map { |snowflake| SnowFlake.new(snowflake) }
+end
+
+
+class SnowFlake
+  attr_reader :arms
+
+  def initialize(data)
+    @arms
+  end
+  
+end
--- spec/algorithmic_thinking/cco07p2_spec.rb
+++ spec/algorithmic_thinking/cco07p2_spec.rb
@@ -3,16 +3,31 @@ require './lib/algorithmic_thinking/cco07p2'
 
 RSpec.describe 'Snowflakes' do
 
-  describe '.snowflake class' do
-    it 'exists and has attributes' 
-    it 'each integers value cant be less than zero'
-    it 'each integers value cant be more than 10_000_000'
+  describe 'compare(snowflake_count, snowflake_desciptions)' do
+    it 'returns response if any two or more snowflakes are the same' do
+      snowflake_count = 2
+      snowflakes = ["1 2 3 4 5 6", "4 3 2 1 6 5"]
+
+      expect(compare(snowflake_count, snowflakes)).to eq('Twin snowflakes found')
+    end
+
+    it 'returns response if the snowflakes are unique' do
+      snowflake_count = 2
+      snowflakes = ["1 2 3 4 5 6", "4 3 2 1 8 5"]
+
+      expect(compare(snowflake_count, snowflakes)).to eq('No two snowflakes are alike.')
+    end
+
+    it 'snowflake_count cant be larger than 100_000'
   end
 
-  describe '#compare(snowflake_count, snowflake_desciptions)' do
-    it 'returns response if any two or more snowflakes are the same'
-    it 'returns response if the snowflakes are unique'
-    it 'snowflake_count cant be larger than 100_000'
+  describe 'snowflake class' do
+    it 'exists and has attributes' do
+      
+    end
+    
+    it 'each integers value cant be less than zero'
+    it 'each integers value cant be more than 10_000_000'
   end
 
 end
