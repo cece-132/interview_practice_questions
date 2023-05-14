0965feede895a64738113b43064ed2bb67a5c052
Candace Eckels <candace.eckels@gmail.com>
2023-05-14 17:01:12 -0600

    Added hints

--- lib/leetcode/remove_duplicates_from_sorted_array.rb
+++ lib/leetcode/remove_duplicates_from_sorted_array.rb
@@ -73,3 +73,24 @@ end
 # end
 
 # does not pass all the test cases so is inefective
+
+# Hint 1
+# In this problem, 
+# the key point to focus on is the input array being sorted. 
+# As far as duplicate elements are concerned, 
+# what is their positioning in the array when the given array is sorted? 
+# Look at the image above for the answer.
+# If we know the position of one of the elements,
+# do we also know the positioning of all the duplicate elements?
+
+# Hint 2
+# We need to modify the array in-place and the size of the final
+# array would potentially be smaller than the size of the input array. 
+# So, we ought to use a two-pointer approach here. 
+# One, that would keep track of the current element in the original
+# array and another one for just the unique elements.
+
+# Hint 3
+# Essentially, once an element is encountered,
+# you simply need to bypass its duplicates and move on to the next unique element.
+
