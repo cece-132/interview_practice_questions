0fa8fc79f09c44217163e90b6118870955389a25
Candace Eckels <candace.eckels@gmail.com>
2023-05-14 16:19:25 -0600

    Added a solution to leetcode interview questions

---
+++ lib/leetcode/remove_duplicates_from_sorted_array.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+# the solution works but isn't as efficient as it could be
+
+def remove_duplicates(nums)
+  return 0 if nums.empty?
+  # accounts for empty arrays
+    
+  k = 1
+  # k starts at 1 because that should be the lowest amount of uniq values
+  # keeps track of the amount of unique values
+    
+  (1...nums.length).each do |i|
+  # starts at 1 because then we can compare the two positions and through the entire length of the array
+      
+    if nums[i] != nums[i - 1]
+     # if nums[at position i] is not equal to nums[at position i minus 1]
+     # meaning if the num we are looking at is not equal to the previous   
+        
+      nums[k] = nums[i]
+      # then we want nums[at position k] to equal nums[at position i]
+        
+      k += 1
+      # increase the unique number count   
+    end
+  end
+  
+  k
+  # returns the unique number count value
+end
+
+
+
